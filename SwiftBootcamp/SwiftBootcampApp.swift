8ebbc1966d68e8eb8b7b5ba8503c4497c106bf42
Mateusz Brychczynski <mat.brych@gmail.com>
2023-08-03 14:18:37 +0100

    First Commit

---
+++ SwiftBootcamp/SwiftBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftBootcampApp.swift
+//  SwiftBootcamp
+//
+//  Created by Mateusz Brychczynski on 03/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
