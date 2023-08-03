ff56a1fbaa7e68fae88dab263ceeab89df48f590
Mateusz Brychczynski <mat.brych@gmail.com>
2023-08-03 15:14:17 +0100

    If Statements

---
+++ SwiftBootcamp/SwiftBasics/IfStatements.swift
@@ -0,0 +1,40 @@
+//
+//  IfStatements.swift
+//  SwiftBootcamp
+//
+//  Created by Mateusz Brychczynski on 03/08/2023.
+//
+
+import Foundation
+
+// if statements
+
+var userIsPremium: Bool = true
+
+//if userIsPremium == true {
+//    print("2 - user is premium")
+//}
+// or we can write if userIsPremium { }<- the same
+
+//if userIsPremium {
+//    print("1 - user is premium")
+//}
+
+//if userIsPremium == false {
+//    print("3 - user is NOT premium")
+//}
+
+// or we can write if !userIsPremium { }<- the same
+
+//if !userIsPremium {
+//    print("4 - user is NOT premium")
+//}
+
+
+// if else
+
+//if userIsPremium {
+//    print("Premium")
+//} else {
+//    print("NOT Premium")
+//}
