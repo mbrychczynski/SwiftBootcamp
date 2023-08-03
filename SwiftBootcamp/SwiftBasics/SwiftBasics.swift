b51818d21691c8f919a7910e7956d8db212e2005
Mateusz Brychczynski <mat.brych@gmail.com>
2023-08-03 14:27:24 +0100

    Swift Basics

---
+++ SwiftBootcamp/SwiftBasics/SwiftBasics.swift
@@ -0,0 +1,35 @@
+//
+//  SwiftBasics.swift
+//  SwiftBootcamp
+//
+//  Created by Mateusz Brychczynski on 03/08/2023.
+//
+
+import Foundation
+
+var greeting = "Hello, Swift"
+//print("greeting")
+
+
+// This is a single-line comment
+// Another single-line comment
+
+/*
+ 
+ This is a multi-line comment
+ that goes to
+ multiple lines!
+ 
+ */
+
+// Naming Conventions
+// Camel Case! - in Swift we use Camel Case naming convention
+
+// The first word is lowercased and then the first character in every following word is uppercased
+
+// CORRECT
+let firstGreeting = "Hello, world"
+let thisIsMyFirstGreeting = "Hello, world"
+
+// WRONG - hard to read
+let thisismysecondgreeting = "Hello, world"
